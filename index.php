464b5be7c17d43c95d52125afa1fe2a344a21ecc
Mackay12 <87673239+Mackay12@users.noreply.github.com>
2021-07-19 22:58:00 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Royal Deserve</title>
+    <link stylesheet="style.css" src="styling/style.css">
+  </head>
+  <body>
+    <div class="container">
+      <form method="post">
+      <input type="text" name="name">
+      </form>
+    </div>
+  </body>
+</html>
